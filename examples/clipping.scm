336cca2801bc3e9c0d1087a635c47411eb679a25
Meowster <142757105+meowstr@users.noreply.github.com>
2026-01-27 17:55:57 -0500

    tabs to spaces

--- examples/clipping.scm
+++ examples/clipping.scm
@@ -24,18 +24,18 @@
        (clear-background RAYWHITE)
        (draw-text "Hold SPACE to use TTF generated font" 20 20 20 LIGHTGRAY)
        (with-scissor-mode
-	30 30 500 200
+        30 30 500 200
         (lambda ()
-	  ;; (begin-scissor-mode 80 30 500 500)
-	  (let ((font   (if use-ttf font-bm font-ttf))
-		(color  (if use-ttf MAROON LIME)))
-	    (draw-text-ex font msg (make-vec2 20.0 100.0)(font-base-size font) 2 color)
-	    (draw-text (if use-ttf
-			   "Using BMFfont (Anglecode) imported"
-			   "Using TTF font generated")
-		       20 (- (get-screen-height) 30) 20 GRAY)))
-	  ;; (end-scissor-mode)
-	  )))
+          ;; (begin-scissor-mode 80 30 500 500)
+          (let ((font   (if use-ttf font-bm font-ttf))
+                (color  (if use-ttf MAROON LIME)))
+            (draw-text-ex font msg (make-vec2 20.0 100.0)(font-base-size font) 2 color)
+            (draw-text (if use-ttf
+                           "Using BMFfont (Anglecode) imported"
+                           "Using TTF font generated")
+                       20 (- (get-screen-height) 30) 20 GRAY)))
+          ;; (end-scissor-mode)
+          )))
     (unless (window-should-close?)
       (loop))))
 
